f8944b40127f6156ad6867516bfab0e075e03374
404-error-404 <1791781644@qq.com>
2020-03-09 22:39:42 +0800

    添加项目文件。

---
+++ socket_server/socket_server.cpp
@@ -0,0 +1,55 @@
+﻿// socket_server.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <stdio.h>
+#include <winsock2.h>
+#pragma comment (lib, "ws2_32.lib")  //加载 ws2_32.dll
+
+int main() {
+    //初始化 DLL
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    //创建套接字
+    SOCKET servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    //绑定套接字
+    sockaddr_in sockAddr;
+    memset(&sockAddr, 0, sizeof(sockAddr));  //每个字节都用0填充
+    sockAddr.sin_family = PF_INET;  //使用IPv4地址
+    sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");  //具体的IP地址
+    sockAddr.sin_port = htons(1234);  //端口
+    bind(servSock, (SOCKADDR*)&sockAddr, sizeof(SOCKADDR));
+
+    //进入监听状态
+    listen(servSock, 20);
+
+    //接收客户端请求
+    SOCKADDR clntAddr;
+    int nSize = sizeof(SOCKADDR);
+    SOCKET clntSock = accept(servSock, (SOCKADDR*)&clntAddr, &nSize);
+
+    //向客户端发送数据
+    const char* str = "Hello World!";
+    send(clntSock, str, strlen(str) + sizeof(char), NULL);
+
+    //关闭套接字
+    closesocket(clntSock);
+    closesocket(servSock);
+
+    //终止 DLL 的使用
+    WSACleanup();
+
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
